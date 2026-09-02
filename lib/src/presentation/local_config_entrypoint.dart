b6b5c459ccedc77b0b81df485a6071ae779a8190
Adriano Teles <adriano.telesc@gmail.com>
2026-09-02 08:29:41 -0300

    Fix pop behavior

--- lib/src/presentation/local_config_entrypoint.dart
+++ lib/src/presentation/local_config_entrypoint.dart
@@ -7,9 +7,16 @@ import 'package:local_config/src/presentation/screens/config_editing_screen.dart
 import 'package:local_config/src/presentation/screens/config_listing_screen.dart';
 
 /// The entry point widget for Local Config UI.
-class LocalConfigEntrypoint extends StatelessWidget {
+class LocalConfigEntrypoint extends StatefulWidget {
   /// Creates a entry point for Local Config UI.
   const LocalConfigEntrypoint({super.key});
+
+  @override
+  State<LocalConfigEntrypoint> createState() => _LocalConfigEntrypointState();
+}
+
+class _LocalConfigEntrypointState extends State<LocalConfigEntrypoint> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +30,32 @@ class LocalConfigEntrypoint extends StatelessWidget {
       ],
       child: Theme(
         data: LocalConfigTheme.data,
-        child: Navigator(
-          initialRoute: LocalConfigRoutes.configList,
-          onGenerateRoute: (settings) {
-            return switch (settings.name) {
-              LocalConfigRoutes.configList => MaterialPageRoute(
-                builder: (_) => const ConfigListingScreen(),
-              ),
-              LocalConfigRoutes.configEdit => MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (_) => ConfigEditingScreen(
-                  name: settings.arguments.toString(),
-                ),
-              ),
-              LocalConfigRoutes.configAdd => MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (_) => const ConfigEditingScreen(),
-              ),
-              _ => null,
-            };
+        child: NavigatorPopHandler<Object?>(
+          onPopWithResult: (result) {
+            _navigatorKey.currentState?.pop(result);
           },
+          child: Navigator(
+            key: _navigatorKey,
+            initialRoute: LocalConfigRoutes.configList,
+            onGenerateRoute: (settings) {
+              return switch (settings.name) {
+                LocalConfigRoutes.configList => MaterialPageRoute(
+                  builder: (_) => const ConfigListingScreen(),
+                ),
+                LocalConfigRoutes.configEdit => MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (_) => ConfigEditingScreen(
+                    name: settings.arguments.toString(),
+                  ),
+                ),
+                LocalConfigRoutes.configAdd => MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (_) => const ConfigEditingScreen(),
+                ),
+                _ => null,
+              };
+            },
+          ),
         ),
       ),
     );
